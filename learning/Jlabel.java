9e800ed7ff3bdedd2f790d6a893b222442dfca34
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2025-02-18 15:50:11 +0700

    jlabel 2

--- learning/Jlabel.java
+++ learning/Jlabel.java
@@ -1,11 +1,22 @@
 package learning;
 import javax.swing.JFrame; //gọi class JFrame từ thư viện swing
 import javax.swing.JLabel; //gọi class JLabel từ thư viện swing
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.ImageIcon; //gọi class ImageIcon từ thư viện swing
 
 public class Jlabel {
     public static void main(String[] args) {
+        ImageIcon icon = new ImageIcon("D:/IT/GitHub Projects/ThienTam/learning/test.jpg"); //tạo icon từ file test.jpg
+
         JLabel label = new JLabel(); //tạo label
-        label.setText("Nhà thuốc Thiện Tâm"); //thiết lập text cho label
+        label.setText("UwU... I need some medicines :<"); //thiết lập text cho label
+        label.setIcon(icon); //thiết lập icon cho label
+        label.setHorizontalTextPosition(JLabel.CENTER); //vị trí text theo chiều ngang
+        label.setVerticalTextPosition(JLabel.TOP); //vị trí text theo chiều dọc
+        label.setForeground(Color.orange); //màu chữ
+        label.setFont(new Font("MV Boli", Font.BOLD, 20)); //font chữ
 
         JFrame frame = new JFrame(); //tạo cửa sổ
         frame.setSize(600, 600); //kích thước cửa sổ
